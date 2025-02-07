6bef73c276f08e5d143cb4e60105966e3ca956da
div <nicka18chinese@gmail.com>
2025-02-07 13:48:43 +0800

    Fix footer responsive

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,21 +1,19 @@
 import React from "react";
 import styles from "./Footer.module.css";
-// import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
-    <div id="footer" className={styles.footer_main}>
+    <div className={styles.footer_main}>
       <div className={styles.socials}>
-          <a href="https://github.com/Dave-Projects">Github</a>
-          <a href="https://www.linkedin.com/in/davevisual/">Linkedin</a>
-          <a href="https://www.instagram.com/davve_xoxo/">Twitter</a>
-          <a href="https://www.instagram.com/davve_xoxo/">Instagram</a>
-          <a href="mailto:daveandrew.visual@gmail.com">Email</a>
-
+        <a href="https://github.com/Dave-Projects" className={styles.github}>Github</a>
+        <a href="https://www.linkedin.com/in/davevisual/" className={styles.linkedin}>Linkedin</a>
+        <a href="https://www.twitter.com/davve_xoxo/" className={styles.twitter}>Twitter</a>
+        <a href="https://www.instagram.com/davve_xoxo/" className={styles.instagram}>Instagram</a>
       </div>
-      <div className={styles.subheading_container}>
-        <div className={styles.subheading}>@daveandrew.visual 2024</div>
-      </div>    
+      <div className={styles.footer_text}>
+        <p>@daveandrew.visual 2024</p>
+        <p>Mail : <a href="mailto:daveandrew.visual@gmail.com">daveandrew.visual@gmail.com</a></p>
+      </div>
     </div>
   );
 };
